2cf2b3e4966a9f6dc583801249d03dc5e90fa4d2
João Vitor Pigozzo <jvpigozzo@gmail.com>
2023-12-06 23:50:15 -0300

    add R

---
+++ analise-diamantes.R
@@ -0,0 +1,15 @@
+
+
+#Instalar o pacote ggplot2
+install.packages('ggplot2')
+
+#Ativar o pacote 
+library(ggplot2)
+
+#Leitura dos dados
+df = read.csv(file.choose())
+
+#Criando o gráfico
+ggplot(data=df, aes(x=carat, y=price, colour=clarity)) +
+  geom_point() + 
+  geom_smooth()
